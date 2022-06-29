941d1f1d837943d30cd90c7469078737f52da46e
samahuja642 <samahuja642@gmail.com>
2022-06-29 13:03:02 +0530

    added settings page , navbar dropdown,updation and deletion feature in reminders

--- reminder_app/forms.py
+++ reminder_app/forms.py
@@ -3,6 +3,8 @@ from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaF
 from wtforms.validators import Email,DataRequired,Length,EqualTo,ValidationError
 from reminder_app.models import User
 import datetime
+from flask_wtf.file import FileField,FileAllowed
+from flask_login import current_user
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
@@ -17,13 +19,13 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),Length(min=2,max=20),EqualTo('password',"Data should match with password's data")])
     phone_number = StringField('Phone Number (with country code)',validators=[DataRequired(),Length(min=2)])
     submit = SubmitField('Sign Up')
-    def validate_email(FlaskForm,email):
+    def validate_email(self,email):
         if User.query.filter_by(email=email.data).first() != None:
             raise ValidationError('Email Already Taken')
-    def validate_username(FlaskForm,username):
+    def validate_username(self,username):
         if User.query.filter_by(username=username.data).first() != None:
             raise ValidationError('Username Already Taken')
-    def validate_phone_number(FlaskForm,phone_number):
+    def validate_phone_number(self,phone_number):
         if User.query.filter_by(phone_number=phone_number.data).first() != None:
             raise ValidationError('Phone Number Already Taken')
 
@@ -46,3 +48,32 @@ class NewReminder(FlaskForm):
 class EmailVerification(FlaskForm):
     otp = IntegerField('OTP :',validators=[DataRequired()])
     submit = SubmitField('Verify Email')
+
+class AccountUpdationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2)])
+    phone_number = StringField('Phone Number (with country code)',validators=[DataRequired(),Length(min=2)])
+    image = FileField('Profile Photo',validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Submit')
+    def validate_username(self,username):
+        if User.query.filter_by(username=username.data).first():
+            if current_user.username != username.data:
+                raise ValidationError('Username Already Taken')
+    def validate_phone_number(self,phone_number):
+        if User.query.filter_by(phone_number=phone_number.data).first() != None:
+            if current_user.phone_number != phone_number.data:
+                raise ValidationError('Phone Number Already Taken')
+
+class ReminderUpdationForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    message = TextAreaField('Reminder',validators=[DataRequired()])
+    date = DateField('Date',validators=[DataRequired()], format='%Y-%m-%d')
+    time = TimeField('Time',validators=[DataRequired()],format='%H:%M')
+    submit = SubmitField('Update Reminder')
+
+    def validate_date(self,date):
+        if datetime.datetime.now().date() > date.data:
+            raise ValidationError("You can't set date back in time")
+    def validate_time(self,time):
+        if datetime.datetime.now().date() == self.date.data:
+            if (datetime.datetime.now() + datetime.timedelta(minutes=30)).time() > time.data:
+                raise ValidationError("You can set atleast after 30 min from now")
--- reminder_app/routes.py
+++ reminder_app/routes.py
@@ -1,12 +1,24 @@
 from reminder_app import app,bcrypt,db,mail
-from reminder_app.forms import LoginForm,RegistrationForm,NewReminder,EmailVerification
+from reminder_app.forms import LoginForm,RegistrationForm,NewReminder,EmailVerification,AccountUpdationForm,ReminderUpdationForm
 from flask import render_template,redirect,url_for,abort,flash,request,session
 from flask_login import login_user,login_required,current_user,logout_user
 from reminder_app.models import User,Reminder
 from datetime import datetime
 import random
 from flask_mail import Message
-import os,time
+import os,time,secrets
+from PIL import Image
+
+def image_saver(form_picture):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics/',picture_fn)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
 
 @app.route('/home',methods=['POST','GET'])
 @login_required
@@ -18,6 +30,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/settings',methods=['POST','GET'])
+@login_required
+def settings():
+    form = AccountUpdationForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.phone_number = form.phone_number.data
+        if form.image.data:
+            print("samarth")
+            current_user.image_file = image_saver(form.image.data)
+        db.session.commit()
+        return redirect(url_for('settings'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.phone_number.data = current_user.phone_number
+    return render_template('settings.html',form=form)
+    
 
 @app.route('/<string:username>/new_reminder',methods=['POST','GET'])
 @login_required
@@ -85,4 +115,45 @@ def logout():
     if current_user.is_authenticated== True:
         logout_user()
         return redirect(url_for('login'))
+
+@app.route('/reminder/<string:id>')
+@login_required
+def show_reminder(id):
+    reminder = Reminder.query.get_or_404(id)
+    if current_user != reminder.author:
+        return abort(403)
+    return render_template("reminder.html",reminder=reminder)
     
+@app.route('/update/<string:id>',methods=['POST','GET'])
+@login_required
+def update_reminder(id):
+    form = ReminderUpdationForm()
+    reminder = Reminder.query.get_or_404(id)
+    if current_user != reminder.author:
+        return abort(403)
+    if form.validate_on_submit():
+        reminder.title = form.title.data
+        reminder.message = form.message.data
+        reminder.date = form.date.data
+        reminder.time = form.time.data
+        db.session.commit()
+        flash('Your Reminder has been Updated','success')
+        return redirect(url_for('update_reminder',id=id))
+    elif request.method == 'GET':
+        form.title.data = reminder.title
+        form.message.data = reminder.message
+        form.date.data = reminder.date
+        form.time.data = reminder.time
+    return render_template('update_reminder.html',form=form)
+
+
+@app.route('/delete/<string:id>',methods=['POST'])
+@login_required
+def delete_reminder(id):
+    reminder = Reminder.query.get_or_404(id)
+    if current_user != reminder.author:
+        abort(403)
+    db.session.delete(reminder)
+    db.session.commit()
+    flash('Your Reminder Has Been Delete!','success')
+    return redirect(url_for('home'))
